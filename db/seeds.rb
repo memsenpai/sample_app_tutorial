05b8c898688a25194c4e44288c070a337016cb79
memsenpai <memsenpai3@gmail.com>
2017-07-03 12:29:18 +0700

    usermiropost 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,16 @@ User.create! name:  "dao van hung",
   name  = Faker::Name.name
   email = "example-#{n + 1}@railstutorial.org"
   password = "password"
-  User.create! name:  name,
+  User.create! name: name,
     email: email,
     password: password,
     password_confirmation: password
+end
+
+users = User.order(:created_at).take(6)
+50.times do |i|
+  content = Faker::Lorem.sentence(5)
+  users.each do |user|
+    user.microposts.create! content: content, created_at: i.minutes.ago
+  end
 end
